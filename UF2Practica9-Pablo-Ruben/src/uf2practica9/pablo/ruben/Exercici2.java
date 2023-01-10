9a7911352e96ad2bef8c0f5a3a64c257a02164bd
a22pabjimpri <121022097+a22pabjimpri@users.noreply.github.com>
2023-01-10 12:12:43 +0100

    Exercici 2 amd els utils

--- UF2Practica9-Pablo-Ruben/src/uf2practica9/pablo/ruben/Exercici2.java
+++ UF2Practica9-Pablo-Ruben/src/uf2practica9/pablo/ruben/Exercici2.java
@@ -1,39 +1,28 @@
 //Implementar un programa que façi l'algorisme d'euclides de manera recursiva
 package uf2practica9.pablo.ruben;
 
-import java.util.Scanner;
+import utils.Utils;
 
 public class Exercici2 {
 
-    public static Scanner scan = new Scanner(System.in);
+   
 
     public static int euclides(int numA, int numB) {
         if (numB==0)
             return numA;  
         else 
-            return euclides(numB, numA%numB);
-        
-
+            return euclides(numB, numA%numB);     
     }
 
     public static void main(String[] args) {
-        int numeroA = escanearEntero("Introdueix el numero A: ");
-        int numeroB = escanearEntero("Introdueix el numero B: ");
+        int numeroA = Utils.LlegirInt("Introdueix el numero A: ");
+        int numeroB = Utils.LlegirInt("Introdueix el numero B: ");
         int resultat = euclides(numeroA, numeroB);
         
         System.out.println(resultat);
     }
 
-    static int escanearEntero(String msj) {
-        int num;
-        System.out.print(msj);
-        while (!scan.hasNextInt()) {
-            scan.next();
-            System.out.println("Ha de ser enter.");
-            System.out.print(msj);
-        }
-        num = scan.nextInt();
-        return num;
-    }
+    
 
 }
+
