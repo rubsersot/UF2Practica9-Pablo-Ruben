a962d01498000b531e801e9bd17dd8fbe250d007
a22pabjimpri <121022097+a22pabjimpri@users.noreply.github.com>
2023-01-10 11:40:17 +0100

    Exercici2

--- UF2Practica9-Pablo-Ruben/src/uf2practica9/pablo/ruben/Exercici2.java
+++ UF2Practica9-Pablo-Ruben/src/uf2practica9/pablo/ruben/Exercici2.java
@@ -1,20 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+//Implementar un programa que façi l'algorisme d'euclides de manera recursiva
 package uf2practica9.pablo.ruben;
 
-/**
- *
- * @author ausias
- */
+import java.util.Scanner;
+
 public class Exercici2 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static Scanner scan = new Scanner(System.in);
+
+    public static int euclides(int numA, int numB) {
+        if (numB==0)
+            return numA;  
+        else 
+            return euclides(numB, numA%numB);
+        
+
     }
-    
+
+    public static void main(String[] args) {
+        int numeroA = escanearEntero("Introdueix el numero A: ");
+        int numeroB = escanearEntero("Introdueix el numero B: ");
+        int resultat = euclides(numeroA, numeroB);
+        
+        System.out.println(resultat);
+    }
+
+    static int escanearEntero(String msj) {
+        int num;
+        System.out.print(msj);
+        while (!scan.hasNextInt()) {
+            scan.next();
+            System.out.println("Ha de ser enter.");
+            System.out.print(msj);
+        }
+        num = scan.nextInt();
+        return num;
+    }
+
 }
